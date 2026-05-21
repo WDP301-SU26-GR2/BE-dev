df568891271a8dc5c6cff9e1e88aaa3db9d2d926
Chill <baophi852@gmail.com>
2026-05-21 16:14:10 +0700

    fix prisma

--- src/shared/helper/helper.prisma.ts
+++ src/shared/helper/helper.prisma.ts
@@ -1,10 +1,10 @@
-import { Prisma } from '@prisma/client'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 
 // FIle này định nghĩa các hàm helper để kiểm tra lỗi của Prisma, ví dụ như lỗi vi phạm ràng buộc duy nhất (unique constraint violation) hoặc lỗi không tìm thấy bản ghi (not found error). Các hàm này sẽ giúp cho việc xử lý lỗi trong service trở nên dễ dàng hơn và mã nguồn trở nên sạch hơn.
-export function isUniqueConstrainError(error: any): error is Prisma.PrismaClientKnownRequestError {
-  return error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2002'
+export function isUniqueConstrainError(error: any): error is PrismaClientKnownRequestError {
+  return error instanceof PrismaClientKnownRequestError && error.code === 'P2002'
 }
 
-export function isNotFoundError(error: any): error is Prisma.PrismaClientKnownRequestError {
-  return error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025'
+export function isNotFoundError(error: any): error is PrismaClientKnownRequestError {
+  return error instanceof PrismaClientKnownRequestError && error.code === 'P2025'
 }
