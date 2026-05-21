cd4168073a9614829b28b9b50d9aaa2fd1a0262f
Chill <baophi852@gmail.com>
2026-05-21 16:14:10 +0700

    fix prisma

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ZodValidationPipe } from 'nestjs-zod'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import envConfig from 'src/shared/config/envConfig'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ZodValidationPipe())
-
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory)
   await app.listen(envConfig.PORT ?? 3000)
 }
 bootstrap()
