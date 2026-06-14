c0bc8aa8a4a22e2a24f7c6462a6e907629e17298
Chill <baophi852@gmail.com>
2026-06-14 19:28:34 +0700

    refactor(shared): consolidate filters/pipe/response dto into http/

--- src/shared/dto/request.dto.ts
+++
@@ -1,4 +0,0 @@
-import { createZodDto } from 'nestjs-zod'
-import { EmptyBodySchema } from 'src/shared/models/shared-request.model'
-
-export class EmptyBodyDto extends createZodDto(EmptyBodySchema) {}
--- src/shared/http/response.dto.ts
+++ src/shared/http/response.dto.ts
@@ -1,6 +1,9 @@
+import { createZodDto } from 'nestjs-zod'
 import z from 'zod'
 
 export const ResponseSchema = z.object({
   message: z.string()
 })
 export type ResponseType = z.infer<typeof ResponseSchema>
+
+export class MessageResDto extends createZodDto(ResponseSchema) {}
