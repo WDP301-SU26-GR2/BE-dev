d3cf877192e364b402246a2befa29d644191083c
Chill <baophi852@gmail.com>
2026-06-26 16:57:00 +0700

    refactor mesage in project

--- src/modules/reviews/errors/reviews.errors.ts
+++ src/modules/reviews/errors/reviews.errors.ts
@@ -1,9 +1,7 @@
 import { UnprocessableEntityException } from '@nestjs/common'
+import { ReviewsMessages } from '../reviews.messages'
 
 // Reviewer không được tự đánh giá chính mình.
 export const CannotReviewSelfException = new UnprocessableEntityException([
-  {
-    message: 'Error.CannotReviewSelf',
-    path: 'targetId'
-  }
+  { message: ReviewsMessages.error.cannotReviewSelf, path: 'targetId' }
 ])
